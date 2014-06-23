ad232b95530dffd95fbbf27dd8183b09b79b9988
Webonaute <mdelisle@webonaute.ca>
2014-06-23 12:45:21 -0400

    Add test class

---
+++ Tests/Command/DoctrineFixturesGeneratorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the FOSTwitterBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\TwitterBundle\Tests\DependencyInjection;
+
+use FOS\TwitterBundle\DependencyInjection\FOSTwitterExtension;
+
+class DoctrineFixturesGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @todo.
+     */
+    public function testLoadFailed()
+    {
+       $this->assertTrue(TRUE);
+    }
+
+
+}
