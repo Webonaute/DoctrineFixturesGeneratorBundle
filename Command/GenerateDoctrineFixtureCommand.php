c900db694a0edb707ecda828a2ee43b18c759457
Webonaute <mdelisle@webonaute.ca>
2014-06-27 20:28:48 -0400

    fix mistake

--- Command/GenerateDoctrineFixtureCommand.php
+++ Command/GenerateDoctrineFixtureCommand.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the RedSkyDown package.
+ * This file is part of the Webonaute package.
  *
  * (c) Mathieu Delisle <mdelisle@webonaute.ca>
  *
--- Tool/FixtureGenerator.php
+++ Tool/FixtureGenerator.php
@@ -1,5 +1,5 @@
 <?php
-/* This file is part of the RedSkyDown package.
+/* This file is part of the Webonaute package.
  *
  * (c) Mathieu Delisle <mdelisle@webonaute.ca>
  *
