9ff38a491dd04de6ce0324d9091b3483bb3cda61
webonaute <mdelisle@webonaute.ca>
2015-03-30 14:21:06 -0400

    Sensiolabs improvement

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        //$rootNode = $treeBuilder->root('doctrine_fixture_generator');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
